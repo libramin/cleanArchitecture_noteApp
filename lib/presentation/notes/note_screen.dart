0411958d792bf6b2f3b08a914e7280669dd9b58e
libramin <libramin9@gmail.com>
2022-12-22 15:56:06 +0900

    note_delete&cancel

--- lib/presentation/notes/note_screen.dart
+++ lib/presentation/notes/note_screen.dart
@@ -25,11 +25,11 @@ class NoteScreen extends StatelessWidget {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async{
+        onPressed: () async {
           bool? isSaved = await Navigator.push(context,
               MaterialPageRoute(builder: (_) => const AddEditNoteScreen()));
 
-          if(isSaved != null && isSaved){
+          if (isSaved != null && isSaved) {
             viewModel.onEvent(const NotesEvent.loadNotes());
           }
         },
@@ -41,6 +41,18 @@ class NoteScreen extends StatelessWidget {
           children: state.notes
               .map((note) => NoteItem(
                     note: note,
+                    onDeleteTap: () {
+                      viewModel.onEvent(NotesEvent.deleteNote(note));
+
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: const Text('노트가 삭제되었습니다'),
+                        action: SnackBarAction(
+                            label: '취소',
+                            onPressed: () {
+                              viewModel.onEvent(const NotesEvent.restoreNote());
+                            }),
+                      ));
+                    },
                   ))
               .toList(),
         ),
